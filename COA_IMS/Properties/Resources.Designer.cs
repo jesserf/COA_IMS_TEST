8b0928bc943d2ba73680cbed517c398f22dd8b08
Kimchinana <checherejoy14@gmail.com>
2024-04-30 16:07:59 +0800

    30

--- COA_IMS/Properties/Resources.Designer.cs
+++ COA_IMS/Properties/Resources.Designer.cs
@@ -303,6 +303,16 @@ namespace COA_IMS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap disk {
+            get {
+                object obj = ResourceManager.GetObject("disk", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap document {
             get {
                 object obj = ResourceManager.GetObject("document", resourceCulture);
--- COA_IMS/Screens/UserMaintenancetab.Designer.cs
+++ COA_IMS/Screens/UserMaintenancetab.Designer.cs
@@ -28,54 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gunaElipsePanel1 = new Guna.UI.WinForms.GunaElipsePanel();
-            this.deactbtn = new Guna.UI.WinForms.GunaButton();
-            this.accountbtn = new Guna.UI.WinForms.GunaButton();
-            this.activebtn = new Guna.UI.WinForms.GunaButton();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.gunaElipsePanel1.SuspendLayout();
+            this.accountbtn = new Guna.UI.WinForms.GunaButton();
+            this.deactbtn = new Guna.UI.WinForms.GunaButton();
+            this.activebtn = new Guna.UI.WinForms.GunaButton();
+            this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
             this.flowLayoutPanel1.SuspendLayout();
+            this.gunaPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // gunaElipsePanel1
+            // flowLayoutPanel1
             // 
-            this.gunaElipsePanel1.BackColor = System.Drawing.Color.Transparent;
-            this.gunaElipsePanel1.BaseColor = System.Drawing.Color.White;
-            this.gunaElipsePanel1.Controls.Add(this.panel1);
-            this.gunaElipsePanel1.Controls.Add(this.flowLayoutPanel1);
-            this.gunaElipsePanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gunaElipsePanel1.Location = new System.Drawing.Point(10, 10);
-            this.gunaElipsePanel1.Name = "gunaElipsePanel1";
-            this.gunaElipsePanel1.Radius = 1;
-            this.gunaElipsePanel1.Size = new System.Drawing.Size(1599, 945);
-            this.gunaElipsePanel1.TabIndex = 0;
-            // 
-            // deactbtn
-            // 
-            this.deactbtn.AnimationHoverSpeed = 0.07F;
-            this.deactbtn.AnimationSpeed = 0.03F;
-            this.deactbtn.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(170)))), ((int)(((byte)(82)))));
-            this.deactbtn.BorderColor = System.Drawing.Color.Black;
-            this.deactbtn.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.deactbtn.FocusedColor = System.Drawing.Color.Empty;
-            this.deactbtn.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deactbtn.ForeColor = System.Drawing.Color.White;
-            this.deactbtn.Image = null;
-            this.deactbtn.ImageSize = new System.Drawing.Size(20, 20);
-            this.deactbtn.Location = new System.Drawing.Point(253, 0);
-            this.deactbtn.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
-            this.deactbtn.Name = "deactbtn";
-            this.deactbtn.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.deactbtn.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.deactbtn.OnHoverForeColor = System.Drawing.Color.White;
-            this.deactbtn.OnHoverImage = null;
-            this.deactbtn.OnPressedColor = System.Drawing.Color.Black;
-            this.deactbtn.Size = new System.Drawing.Size(250, 55);
-            this.deactbtn.TabIndex = 1;
-            this.deactbtn.Text = "Deactivated Accounts";
-            this.deactbtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.deactbtn.Click += new System.EventHandler(this.gunaButton1_Click);
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.accountbtn);
+            this.flowLayoutPanel1.Controls.Add(this.deactbtn);
+            this.flowLayoutPanel1.Controls.Add(this.activebtn);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1599, 55);
+            this.flowLayoutPanel1.TabIndex = 3;
             // 
             // accountbtn
             // 
@@ -103,6 +75,32 @@
             this.accountbtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.accountbtn.Click += new System.EventHandler(this.gunaButton1_Click);
             // 
+            // deactbtn
+            // 
+            this.deactbtn.AnimationHoverSpeed = 0.07F;
+            this.deactbtn.AnimationSpeed = 0.03F;
+            this.deactbtn.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(170)))), ((int)(((byte)(82)))));
+            this.deactbtn.BorderColor = System.Drawing.Color.Black;
+            this.deactbtn.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.deactbtn.FocusedColor = System.Drawing.Color.Empty;
+            this.deactbtn.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deactbtn.ForeColor = System.Drawing.Color.White;
+            this.deactbtn.Image = null;
+            this.deactbtn.ImageSize = new System.Drawing.Size(20, 20);
+            this.deactbtn.Location = new System.Drawing.Point(253, 0);
+            this.deactbtn.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.deactbtn.Name = "deactbtn";
+            this.deactbtn.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.deactbtn.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.deactbtn.OnHoverForeColor = System.Drawing.Color.White;
+            this.deactbtn.OnHoverImage = null;
+            this.deactbtn.OnPressedColor = System.Drawing.Color.Black;
+            this.deactbtn.Size = new System.Drawing.Size(250, 55);
+            this.deactbtn.TabIndex = 1;
+            this.deactbtn.Text = "Deactivated Accounts";
+            this.deactbtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.deactbtn.Click += new System.EventHandler(this.gunaButton1_Click);
+            // 
             // activebtn
             // 
             this.activebtn.AnimationHoverSpeed = 0.07F;
@@ -129,25 +127,15 @@
             this.activebtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.activebtn.Click += new System.EventHandler(this.gunaButton1_Click);
             // 
-            // flowLayoutPanel1
+            // gunaPanel1
             // 
-            this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.Controls.Add(this.accountbtn);
-            this.flowLayoutPanel1.Controls.Add(this.deactbtn);
-            this.flowLayoutPanel1.Controls.Add(this.activebtn);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1599, 55);
-            this.flowLayoutPanel1.TabIndex = 3;
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 55);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1599, 890);
-            this.panel1.TabIndex = 4;
+            this.gunaPanel1.BackColor = System.Drawing.Color.White;
+            this.gunaPanel1.Controls.Add(this.flowLayoutPanel1);
+            this.gunaPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gunaPanel1.Location = new System.Drawing.Point(10, 10);
+            this.gunaPanel1.Name = "gunaPanel1";
+            this.gunaPanel1.Size = new System.Drawing.Size(1599, 945);
+            this.gunaPanel1.TabIndex = 4;
             // 
             // UserMaintenancetab
             // 
@@ -155,25 +143,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(170)))), ((int)(((byte)(82)))));
             this.ClientSize = new System.Drawing.Size(1619, 965);
-            this.Controls.Add(this.gunaElipsePanel1);
+            this.Controls.Add(this.gunaPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "UserMaintenancetab";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "UserMaintenancetab";
-            this.gunaElipsePanel1.ResumeLayout(false);
-            this.gunaElipsePanel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.gunaPanel1.ResumeLayout(false);
+            this.gunaPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel1;
         private Guna.UI.WinForms.GunaButton accountbtn;
         private Guna.UI.WinForms.GunaButton deactbtn;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private Guna.UI.WinForms.GunaButton activebtn;
+        private Guna.UI.WinForms.GunaPanel gunaPanel1;
     }
 }
